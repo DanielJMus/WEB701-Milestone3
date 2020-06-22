34d2331e014a3567f2639c0f1368b1818d957fe1
DanielJMus <danielmus1999@hotmail.com>
2020-06-22 12:13:52 +1200

    Added chat functionality, you can now log in by talking to the chat bot

--- client/src/Components/Chat.js
+++ client/src/Components/Chat.js
@@ -2,10 +2,93 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ChatBot from 'react-simple-chatbot';
 
+import { connect } from 'react-redux';
+import { login } from '../Actions/action';
+
 import './Chat.css';
 
+class EmailCheck extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      trigger: false,
+      triggerValue: ""
+    };
+    this.triggerNext = this.triggerNext.bind(this);
+  }
+
+  triggerNext () {
+    this.setState({trigger: true}, () => {
+        this.props.triggerNextStep({trigger:this.state.triggerValue});
+    });
+  }
+
+  componentWillMount () {
+    const { steps } = this.props;
+    const email = steps.email.value;
+    fetch('http://localhost:4200/api/users/' + email, {
+      method: 'get'
+    }).then(res =>
+      res.json().then(json => {
+        console.log(json);
+          if(json.length > 0) this.setState({triggerValue:"email-success"})
+          else this.setState({triggerValue:"email-fail"});
+          this.triggerNext();
+      })
+    );
+  }
+
+  render() {
+    return null
+  }
+}
+
+class PasswordCheck extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      trigger: false,
+      triggerValue: ""
+    };
+    this.triggerNext = this.triggerNext.bind(this);
+  }
+
+  triggerNext () {
+    this.setState({trigger: true}, () => {
+        this.props.triggerNextStep({trigger:this.state.triggerValue});
+    });
+  }
+
+  componentWillMount () {
+    const { steps } = this.props;
+    const email = steps.email.value;
+    const password = steps.password.value;
+    fetch('http://localhost:4200/api/users/' + email, {
+      method: 'get'
+    }).then(res =>
+      res.json().then(json => {
+          if(json.length > 0) {
+              if(json[0].PASSWORD === password) {
+                  this.setState({triggerValue: 'password-success'});
+              } else {
+                this.setState({triggerValue: 'password-fail'});
+              }
+          }
+          this.triggerNext();
+      })
+  );
+  }
+
+  render() {
+    return null
+  }
+}
+
+
+
+
 // List of queries, synonyms are separated with a | symbol
-const loginQueries = ["can't|cannot|cant|trying to", "log in|login"]
+const loginQueries = ["struggling to|can't|cannot|cant|trying to", "log in|login|log into"]
 
 // Returns a percentage to 100 of confidence that user is making a specific query
 function CheckConfidence (input, keywords)
@@ -31,12 +114,23 @@ function CheckMessage (steps)
   console.log("Login confidence: " + confidence);
   if(confidence >= 50)
   {
-    return ["It looks like you are trying to log in", "login"];
+    return ["Please tell me your email address and I will check if it is valid", "email"];
   }
   return userInput;
 }
 
 class Chat extends Component {
+
+    
+    constructor(props) {
+      super(props);
+    }
+
+    Login = (steps) => {
+      this.props.login(steps.email.value, steps.password.value);
+      return "Done! You should now be logged in :)";
+    }
+
 
     render () {
         return (
@@ -63,21 +157,86 @@ class Chat extends Component {
           {
             id: 'userQuery',
             user: true,
-            trigger: 'results'
+            trigger: 'queryResult'
           },
           {
-            id: 'login',
-            message: "I will now help you log into your account",
-            trigger: 'confirm'
-          },
-          {
-            id: 'results',
+            id: 'queryResult',
             message: ({steps}) => CheckMessage(steps)[0],
             trigger: ({steps}) => CheckMessage(steps)[1]
           },
+          // LOGIN BRANCH
           {
-            id: 'confirm',
-            message: "Is this helpful?",
+            id: 'email',
+            user: true,
+            trigger: 'email-check'
+          },
+          {
+            id: 'email-check',
+            component: <EmailCheck/>,
+            waitAction: true,
+            asMessage: true,
+            replace: true,
+            trigger: ''
+          },
+          {
+            id: 'email-success',
+            message: "That email is valid, please enter your password",
+            trigger: 'password'
+          },
+          {
+            id: 'email-fail',
+            message: "Hmm, that email does not exist in our site, try entering it again",
+            trigger: 'email'
+          },
+          {
+            id: 'password',
+            user: true,
+            trigger: 'password-check'
+          },
+          {
+            id: 'password-check',
+            component: <PasswordCheck/>,
+            waitAction: true,
+            asMessage: true,
+            replace: true,
+            trigger: ''
+          },
+          {
+            id: 'password-success',
+            message: "Your credentials are correct, would you like me to log you in?",
+            trigger: 'login-check'
+          },
+          {
+            id: 'password-fail',
+            message: "That password seems to be incorrect, try entering it again",
+            trigger: 'password'
+          },
+          {
+            id: 'login-check',
+            options: [
+              { value: 'yes', label: 'Yes', trigger: 'login-yes'},
+              { value: 'no', label: 'No', trigger: 'login-no'}
+            ],
+          },
+          {
+            id: 'login-yes',
+            message: ({steps}) => this.Login(steps),
+            trigger: 'confirm-1'
+          },
+          {
+            id: 'login-no',
+            message: "ok :)",
+            trigger: 'confirm-1'
+          },
+
+
+          {
+            id: 'confirm-1',
+            message: "Was this helpful?",
+            trigger: "confirm-2"
+          },
+          {
+            id: 'confirm-2',
             options: [
               { value: 'yes', label: 'Yes', trigger: 'confirm-yes'},
               { value: 'no', label: 'No', trigger: 'confirm-no'}
@@ -100,4 +259,19 @@ class Chat extends Component {
     }
 }
 
-export default Chat;
+const mapStateToProps = (state) => {
+  return {
+      isLoginPending: state.isLoginPending,
+      isLoginSuccess: state.isLoginSuccess,
+      isLoginError: state.isLoginError,
+      isSeller : state.isSeller
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      login: (username, password) => dispatch(login(username, password))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Chat);
